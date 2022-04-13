3800c2428118b0380304364bc7a19f3bf6337fa2
admin <frysasha@yandex.ru>
2022-04-13 12:34:48 +0300

    Добавлен тест на добавления книги в корзину: решение математической задачи, проверка на корректность названия и стоимости книги

--- pages/locators.py
+++ pages/locators.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 
+
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
 
@@ -7,3 +8,11 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM_LINK = (By.ID, 'login_form')
     REGISTER_FORM_LINK = (By.CSS_SELECTOR, 'register_form')
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET = (By.XPATH, ".//*[@class='btn btn-lg btn-primary btn-add-to-basket']")
+    BOOK_PRICE = (By.XPATH, '//*[@id="content_inner"]/article/div[1]/div[2]/p[1]')
+    BOOK_NAME = (By.XPATH, '//*[@id="content_inner"]/article/div[1]/div[2]/h1')
+    BASKET_PRICE = (By.XPATH, '//*[@id="messages"]/div[3]/div/p[1]/strong')
+    ADDED_BOOK_NAME = (By.XPATH, '//*[@id="messages"]/div[1]/div/strong')
---
+++ pages/product_page.py
@@ -0,0 +1,20 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+
+    def add_to_basket(self):
+        add_to_basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
+        add_to_basket_button.click()
+
+
+    def should_be_correct_name(self):
+        book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
+        basket_price = self.browser.find_element(*ProductPageLocators.BASKET_PRICE).text
+        assert book_price == basket_price, 'Цена книги отличается от суммы в корзине'
+
+    def should_be_correct_prise(self):
+        book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
+        added_book_name = self.browser.find_element(*ProductPageLocators.ADDED_BOOK_NAME).text
+        assert book_name == added_book_name, 'Название книги отличается от названии книги в корзине'
